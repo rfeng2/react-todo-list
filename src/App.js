64d0aa858bf219c00007abb4ac1969c512fd7162
Ryan Feng <ryanfeng@Ryans-MacBook-Pro.local>
2020-08-18 02:16:09 -0400

    Styling

--- src/App.js
+++ src/App.js
@@ -51,8 +51,8 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
-          <div className="col-10.mx-auto.col-md-8.mt-4">
+        <div className="row justify-content-center">
+          <div className="col-10 mx-auto col-md-8 mt-4">
             <h3 className="text-center text-capitalize"> to do input</h3>
             <TodoInput
               item={this.state.item}
